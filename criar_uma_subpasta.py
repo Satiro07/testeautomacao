529a2d36de42db9948a93226bf432f57235e2798
Satiro07 <jardelsatiro57@gmail.com>
2025-03-16 11:15:01 -0300

    .

---
+++ criar_uma_subpasta.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+
+desktop = r'C:\Users\José Satiro\OneDrive\Desktop'
+
+conteudo = input('Digite algo: ')
+
+arquivo_caminho = os.path.join(desktop, 'arquivo.py')
+print(arquivo_caminho)
+
+
+with open(arquivo_caminho, 'w') as arquivo:
+    arquivo.write(conteudo)
+
+if os.path.exists(arquivo_caminho):
+    pasta = os.path.join(desktop, 'PastaR')
+    os.makedirs(pasta, exist_ok=True)
+
+    arq_destino = os.path.join(pasta, 'Subpasta')
+    
+    arq = os.path.join(arq_destino, 'arquivo.py')
+    shutil.move(arquivo_caminho, arq)
+    
+    print('Deu certo')
+else:
+    print('Não deu certo')
